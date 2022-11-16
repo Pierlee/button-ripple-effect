ba606b508169092ef09c765c1c18534fbd110f72
Pierlee Franklin Costa Guimaraes <79582249+Pierlee@users.noreply.github.com>
2022-11-16 12:01:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (evento)=>{
+  const x = (evento.pageX - btnEl.offsetLeft);
+  const y = (evento.pageY - btnEl.offsetTop);
+
+  btnEl.style.setProperty("--xPos", x + "px")
+  btnEl.style.setProperty("--yPos", y + "px")
+})
